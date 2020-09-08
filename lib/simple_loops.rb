01536944b8055fe48619ec0c0c4ed23fe6b1dc59
Justin Clifford <justoon7.jc@gmail.com>
2020-09-08 22:10:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -9,8 +9,8 @@ end
 
 def loop_message_n_times(message, number)
   count = 0
-  message = "Hello Moon."
-  number = 5
+  message = "Hello Red Balloon."
+  number = 10
   while count <= number do
     puts message
     count += 1
